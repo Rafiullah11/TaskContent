74517000b066686c8bea187cab2a5c6660c3b6be
Rafiullah <rafiullahafridi11@yahoo.com>
2022-06-01 12:50:59 +0500

    Some changing and handel some issue

--- TaskContent/Controllers/EmployeesController.cs
+++ TaskContent/Controllers/EmployeesController.cs
@@ -6,31 +6,45 @@ namespace TaskContent.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeesController : ControllerBase
+    public class EmployeesController : Controller
     {
         [HttpGet(Name = "GetEmployee")]
         public IEnumerable<Employee> Get()
-        {
-            if (HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
-            {
-                //here we will return View or full view
+        { //here we will return View or full view
                 var employees = new List<Employee>
                 {
                 new Employee{Id=1, Name="Rafi",City="Peshawar"},
                 new Employee{Id=2, Name="Ali",City="Abbotabad"},
                 new Employee{Id=3, Name="Wali",City="Islamabad"},
                 };
-            return employees;
 
+                return employees;
 
+        }
+        [HttpGet(Name = "ViewMethod")]
+        public IActionResult ViewMethod()
+        {
+            if (HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
+            {
+                return View();
             }
             else
             {
-                //here we will return Partial View 
-
-                return Enumerable.Empty<Employee>();        
+                return PartialView(HttpContext.Request);
             }
-             
+        }
+        [HttpGet(Name = "GetMethod")]
+        public IActionResult GetMethod()
+        {
+            if (HttpContext.Request.Headers["Accept"] == "application/json")
+                return Ok();
+            else if (HttpContext.Request.Headers["Accept"] == "application/xml")
+                return Ok();
+            else if (HttpContext.Request.Headers["Accept"] == "text/plain")
+                return Ok();
+            return Ok();
+
+
         }
 
     }
--- TaskContent/Program.cs
+++ TaskContent/Program.cs
@@ -3,11 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddControllers(option =>
-{
-    option.RespectBrowserAcceptHeader = true;
 
-}).AddXmlSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,5 +23,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
